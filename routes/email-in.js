dff83199c4ac3b5a8e5d0f1ace84088214158dee
Paul Caponetti <paul.caponetti@gmail.com>
2015-09-14 16:38:57 -0400

    linking in route

--- routes/email-in.js
+++ routes/email-in.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/email/in', function(req, res, next) {
+router.post('/email/in', function(req, res, next) {
   console.log(req.body);
   res.send(req.body);
 });
