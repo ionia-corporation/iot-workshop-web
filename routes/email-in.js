90bf513251b23067dfe06f0aedd6707b1e2cfa89
Paul Caponetti <paul.caponetti@gmail.com>
2015-09-14 16:49:55 -0400

    more debug

--- routes/email-in.js
+++ routes/email-in.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.post('/email/in', function(req, res, next) {
   console.log(req.body);
+  console.log(JSON.stringify(req.body));
   res.send(req.body);
 });
 
