48f60d64f1c3380cb428cafc60b3aa46c18cc643
Kavi Siegel <hello@kavisiegel.com>
2015-11-11 16:38:27 -0500

    show mqtt credentials on index page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'the Xively Button'
+    title: 'The Xively Button',
+	MQTT_SERVER_ADDR: process.env.XIVELY_BROKER_HOST,
+	MQTT_USERNAME_S: process.env.XIVELY_ACCOUNT_BROKER_USER,
+	MQTT_KEY: process.env.XIVELY_ACCOUNT_BROKER_PASSWORD,
+	MQTT_TOPIC: process.env.XIVELY_SAMPLE_DEVICE_CHANNEL,
   });
 });
 
